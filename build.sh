87e14104ab4ff59276c7c50728be5c7e27a577a0
Tony Liu <contactTonyLiu@gmail.com>
2021-05-31 19:31:30 -0700

    renaming

--- build.sh
+++ build.sh
@@ -5,8 +5,35 @@ PYPI=testpypi
 #PYPI=pypi
 
 TOKEN=`cat credentials/${PYPI}`
+packages=(common server inventory)
 
-for package in common client core; do
+# prebuild
+for arg in "$@"; do
+    case $arg in
+    -clean)
+        echo "cleaning previous builds..."
+        wdir=`pwd`
+        for package in "${packages[@]}"; do
+            cd limes_$package
+            rm -rf build
+            rm -rf dist
+            cd src
+            for dir in */; do
+                if [[ $dir == *.egg-info/ ]]; then
+                    rm -rf $dir
+                fi
+            done
+            cd $wdir
+        done
+        ;;
+    *)
+        echo "ignoring unknown option [$arg]"
+        ;;
+    esac
+done
+
+wdir=`pwd`
+for package in "${packages[@]}"; do
     echo "#########################################################"
     echo "building $package"
     echo "#########################################################"
@@ -17,7 +44,24 @@ for package in common client core; do
     cd limes_$package
     
     python -m build
-    python -m twine upload --repository $PYPI dist/* -u __token__ -p $TOKEN
 
-    cd ..
+    cd $wdir
+done
+
+# post build
+for arg in "$@"; do
+    case $arg in
+    -upload)
+        echo "uploading ..."
+        wdir=`pwd`
+        for package in "${packages[@]}"; do
+            cd limes_$package
+            python -m twine upload --repository $PYPI dist/* -u __token__ -p $TOKEN
+            cd $wdir
+        done
+        ;;
+    *)
+        echo "ignoring unknown option [$arg]"
+        ;;
+    esac
 done
