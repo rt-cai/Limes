68498f267b3b938dfdeafd47d5cd90d62c846573
Tony Liu <contactTonyLiu@gmail.com>
2021-11-23 14:07:54 -0800

    server dependencies

--- conda-install.sh
+++ conda-install.sh
@@ -3,6 +3,10 @@
 conda install -y gunicorn
 conda install -y requests
 conda install -y flask
-conda install -y flask-socketio=4.3.2
+conda install -y flask-socketio=4.3.1
+conda install -y python-engineio=3.13.2
+conda install -y python-socketio=4.6.0
+conda install -y gevent-websocket
+
 conda install -y pytz
 conda install -y numpy
