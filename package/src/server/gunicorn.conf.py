56f156ca3466e865da52291cd4a13606f87c3bf7
Tony Liu <contactTonyLiu@gmail.com>
2021-11-23 09:09:04 -0800

    test config

--- package/src/server/gunicorn.conf.py
+++ package/src/server/gunicorn.conf.py
@@ -6,6 +6,6 @@ workers = 1
 max_requests = 1000
 worker_class = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
 
-CERT_DIR = 'server/certificate/certFiles'
-certfile = '%s/cert.pem'%(CERT_DIR)
-keyfile = '%s/key.pem'%(CERT_DIR)
+# CERT_DIR = 'server/certificate/certFiles'
+# certfile = '%s/cert.pem'%(CERT_DIR)
+# keyfile = '%s/key.pem'%(CERT_DIR)
